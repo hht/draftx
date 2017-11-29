4ae7029b89c823d517ddf7eff32afae2337cf300
ava <hht@163.com>
2017-11-29 14:54:47 +0800

    create-react-app

---
+++ config-overrides.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-param-reassign */
+const path = require('path')
+const { getLoader } = require('react-app-rewired')
+
+const ESLINT_PATH = `eslint-loader${path.sep}index.js`
+
+const getEslintOptions = (rules) => {
+  const matcher = rule =>
+    rule.loader &&
+    typeof rule.loader === 'string' &&
+    rule.loader.endsWith(ESLINT_PATH)
+  return getLoader(rules, matcher).options
+}
+
+module.exports = (config, env, override = f => f) => {
+  //* 修改入口文件
+  // config.entry.push(`${__dirname}/src/client/index.js`)
+
+  /* 使用preact替代react
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    react: 'preact-compat',
+    'react-dom': 'preact-compat'
+  }
+  */
+  // *配置项目源文件根目录别名
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': path.resolve(__dirname, './src/editor')
+  }
+
+  //* 使用自定义eslint配置
+  const eslintOptions = getEslintOptions(config.module.rules)
+  eslintOptions.useEslintrc = true
+  eslintOptions.ignore = true
+  override(eslintOptions)
+
+  return config
+}
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome to React and Draftx :P</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+)
